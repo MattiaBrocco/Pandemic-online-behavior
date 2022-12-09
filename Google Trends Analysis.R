8e9368734ca3583761dd26b336268cd5f1a58bc4
ChiaraDelu <106145337+ChiaraDelu@users.noreply.github.com>
2022-12-09 10:59:47 +0100

    Update Google Trends Analysis.R

--- Google Trends Analysis.R
+++ Google Trends Analysis.R
@@ -7,9 +7,6 @@ library(readr)
 Google_Trends <- read_csv("Desktop/Business Data/Google Trends Data.csv", na = "0", skip = 1)
 colnames(Google_Trends) <- c('Time','Amazon','Netflix', 'Zoom')
 
-goo = Google_Trends[2:4]
-trends = ts(goo, freq=365.25/7, start=decimal_date(ymd("2017-12-10")))
-
 plot(ts(Google_Trends[2:4], freq=365.25/7, start=decimal_date(ymd("2017-12-10"))), main = "Google Trends")
 
 amazon = ts(Google_Trends$Amazon, freq=52, start=decimal_date(ymd("2017-12-10")))
