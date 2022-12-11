e5f42afd7e17aa80244d2478f19ca66474627f64
ChiaraDelu <106145337+ChiaraDelu@users.noreply.github.com>
2022-12-11 14:29:50 +0100

    Update Google Trends Analysis.R

--- Google Trends Analysis.R
+++ Google Trends Analysis.R
@@ -15,6 +15,67 @@ z = as.numeric(Google_Trends$Zoom)
 z[is.na(z)] <- 0
 zoom = ts(z, freq=52, start=decimal_date(ymd("2017-12-10")))
 
-autoplot(netflix)
-acf(netflix)
-pacf(netflix)
+###AMAZON
+
+autoplot(amazon)
+acf(amazon, lag.max = 260) #There is seasonality
+pacf(amazon, lag.max = 104)
+
+#Qui ho tolto il 2017 perchè avendo solo 4 osservazioni dava prroblemi di intepretabilità nel plot
+seas_amz= ts(Google_Trends$Amazon[4:260], freq=52, start=decimal_date(ymd("2018-1-6")))
+ggseasonplot(seas_amz, main = "Seasonal Plot: Amazon")
+ggseasonplot(seas_amz, polar=TRUE) +
+  ylab("Amazon Trends") +
+  ggtitle("Amazon Trend: a weekly overview")
+
+#Per maggiore interpretqbilità, potebbe essere un'idea fare questi plot su base mensile
+
+
+#Now we know our data as seasonal. So I need to differencing.
+
+amazon %>% diff(lag=52) %>% ggtsdisplay() #dati ancora stagionali, bisogna differenziare ancora
+amazon %>% diff(lag=52) %>% diff() %>% ggtsdisplay()#Now the ts is without seasonality.
+
+diff_amz <- amazon %>% diff(lag=52) %>% diff() 
+
+diff_amz %>%
+  Arima(order=c(1,0,2), seasonal=c(1,0,0)) %>% residuals() %>% ggtsdisplay()
+
+fit3 <- Arima(diff_amz, order=c(1,0,2), seasonal=c(1,0,0)) 
+checkresiduals(fit3)
+
+plot(diff_amz)
+lines(fitted(fit3), col = 2)
+fit3 %>% forecast(h=52) %>% autoplot()
+
+#Now I try to use the auto.arima function on the original amazon time serie:
+
+amazon %>%
+  auto.arima() %>% residuals() %>% ggtsdisplay()
+
+#As we expected, there is one differencing component and one seasonal differencing component.
+
+fit1 <- Arima(amazon, order=c(1,1,2), seasonal=c(1,1,0))
+checkresiduals(fit1)
+plot(amazon)
+lines(fitted(fit1), col = 2)
+
+#FORECASTING
+amazon %>%
+  Arima(order=c(1,1,2), seasonal=c(1,1,0), lambda=0) %>% forecast() %>%
+  autoplot() +
+  ylab("Amazon Trends") + xlab("Year")
+
+
+###Holt Winters
+
+HW1 <- HoltWinters(amazon, seasonal = "multiplicative")
+#HW2 <- HoltWinters(amazon, seasonal = "additive")
+#HW2 <- HoltWinters(amazon, alpha=0.2, beta=0.1, gamma=0.1)
+plot(amazon, ylab="Amazon Trend")
+lines(HW1$fitted[,1], col="red")
+#lines(HW2$fitted[,1], col="red")
+lines(fitted(fit1), col="blue") #Confronto con modello ARIMA
+
+cat("RMSE of best ARIMA model is", rmse(amazon, fitted(fit1)))
+cat("RMSE of HW model is", rmse(amazon, HW1$fitted[,1]))
